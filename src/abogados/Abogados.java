52d785687794c6d4898b2785d5d368c256b02cfa
Boxvi <Boxvi@DESKTOP-4EKRB4V>
2021-08-25 15:30:53 -0500

    Primer commit creaccion del paquete abogados con su repectiva clase

---
+++ src/abogados/Abogados.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abogados;
+
+/**
+ *
+ * @author Boxvi
+ */
+public class Abogados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic hereç
+        System.out.println("Estamos en el repositorio");
+    }
+    
+}
