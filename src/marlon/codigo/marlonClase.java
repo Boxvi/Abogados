55e6eddc78d482e9e3b08ba3ba1b7e4c9c474b96
Boxvi <Boxvi@DESKTOP-4EKRB4V>
2021-08-25 19:51:15 -0500

    Se agregaron nuevos paquetes

--- src/marlon/codigo/marlonClase.java
+++ src/marlon/codigo/marlonClase.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+se realiza el modulo
  */
 package marlon.codigo;
 
